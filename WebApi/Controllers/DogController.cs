45e98dd8ec0b0c643aeb1071570026d62d552da1
philjhale <philjhale@gmail.com>
2018-12-12 12:32:02 +0000

    Solution setup. Added function app, simple web API and k8s config

---
+++ WebApi/Controllers/DogController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ObservabilitySampleApp.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DogController : ControllerBase
+    {
+        // GET api/dog
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                // https://dog.ceo/dog-api/
+                var response = await client.GetAsync("https://dog.ceo/api/breeds/image/random");
+                
+                response.EnsureSuccessStatusCode();
+                
+                var stringResult = await response.Content.ReadAsStringAsync();
+                return new JsonResult(stringResult);
+            }
+        }
+    }
+}
