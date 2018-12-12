8a8509af371022313a8a2b191f74f97a96814125
philjhale <philjhale@gmail.com>
2018-12-12 15:14:13 +0000

    Added Application Insights instrumentation to WebAPI project

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -19,6 +19,8 @@ namespace ObservabilitySampleApp.WebApi
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>()
+                //https://github.com/Microsoft/ApplicationInsights-aspnetcore/wiki/Getting-Started-for-a-ASP.NET-CORE-2.0-WebApp
+                .UseApplicationInsights(); 
     }
 }
